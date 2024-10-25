59315b5f6c46ed81b0de8d47df1a85276c2aaeed
Sung <sung272012@hotmail.com>
2024-10-25 15:35:39 -0400

    Solution

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get(
+  '/api/:date',
+  (req, res, next) => {
+    req.date = new Date().toString();
+    req.unix = Date.now();
+    next();
+  },
+  (req, res) => {
+    res.json({ unix: req.unix, utc: req.date });
+  }
+);
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
